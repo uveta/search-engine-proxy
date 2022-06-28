c479469625467e9d7c4f1098105e2940941e0893
Uroš Miletić <root@uveta.io>
2022-06-28 20:45:42 +0200

    Accept cookies and output content to file

--- src/Proxy.Engine/app/google.ts
+++ src/Proxy.Engine/app/google.ts
@@ -1,10 +1,21 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.google.com', {
-    waitUntil: 'networkidle2',
+  await page.goto('https://www.google.com/?hl=en', {
+    waitUntil: 'domcontentloaded',
+  });
+  const [button] = await page.$x("//button[contains(., 'Accept all')]");
+  if (button) {
+    await button.click();
+  }
+  const content = await page.content();
+  fs.writeFile('../output/google.html', content, (err) => {
+    if (err) {
+      console.error(err);
+    }
   });
 
   await browser.close();
